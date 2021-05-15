63f9038b53867727b94c463e25e69512be30061f
Purav04 <49310456+Purav04@users.noreply.github.com>
2021-05-15 08:24:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,6 +91,7 @@ def gen_frames():
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
     success,frame = camera.read()
+    print("frame::",frame)
     #print("frame shape::",frame.shape)
     #frame = cv2.resize(frame,(640,480),interpolation=cv2.INTER_AREA)
 
